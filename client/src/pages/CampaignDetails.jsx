a9518e69ec1b09e221fa2fae000e433b18399682
photonwings <vaibhavhathwar404@gmail.com>
2023-06-01 15:30:03 +0530

    Campaign Description: Like button added, Funding campaign moved to top

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -41,17 +41,24 @@ const CampaignDetails = () => {
     setIsLoading(false);
   };
 
+  //! Pending implementation
+  let likes = 21;
+  let isLiked = true;
+  const handleLike = () => {}
+
   return (
     <div>
       {isLoading && <Loader />}
-
+      {/* Upper portion: Image and count boxes */}
       <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+        {/* Image and progress bar */}
         <div className="flex-1 flex-col">
           <img
             src={state.image}
             alt="campaign"
-            className="w-full h-[410px] object-cover rounded-xl"
+            className="w-full h-[410px] object-fill rounded-xl"
           />
+          {/* Progress bar */}
           <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
             <div
               className="absolute h-full bg-[#4acd8d]"
@@ -65,8 +72,8 @@ const CampaignDetails = () => {
             ></div>
           </div>
         </div>
-
-        <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
+        {/* Countbox */}
+        <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[10px]">
           <CountBox
             title="Days Left"
             value={remainingDays <= 0 ? 0 : remainingDays}
@@ -76,6 +83,52 @@ const CampaignDetails = () => {
             value={state.amountCollected}
           />
           <CountBox title="Total Backers" value={donators.length} />
+          <CustomButton
+            btnType="button"
+            title={`${likes} Likes`}
+            styles={`w-[150px] ${isLiked ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}`}
+            handleClick={handleLike}
+          />
+        </div>
+        {/* Funding part */}
+        <div className="flex-1">
+          {/* <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
+            Fund
+          </h4> */}
+
+          <div className="flex flex-col p-4 bg-[#1c1c24] rounded-[10px]">
+            <p className="font-epilogue fount-medium text-[20px] leading-[30px] text-center text-white">
+              Fund the campaign
+            </p>
+            <div className="mt-[30px]">
+              <input
+                required
+                type="number"
+                placeholder="ETH 0.1"
+                step="0.01"
+                className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+
+              <div className="my-[30px] p-4 bg-[#13131a] rounded-[10px]">
+                <h4 className="font-epilogue font-semibold text-[14px] leading-[22px] text-white">
+                  Back it because you believe in it.
+                </h4>
+                <p className="mt-[30px] mb-[30px] font-epilogue font-normal leading-[22px] text-[#808191]">
+                  Support the project for no reward, just because it speaks to
+                  you.
+                </p>
+              </div>
+
+              <CustomButton
+                btnType="button"
+                title="Fund Campaign"
+                styles="w-full bg-[#8c6dfd]"
+                handleClick={handleDonate}
+              />
+            </div>
+          </div>
         </div>
       </div>
 
@@ -142,46 +195,6 @@ const CampaignDetails = () => {
                   No donators yet. Be the first one!
                 </p>
               )}
-            </div>
-          </div>
-        </div>
-
-        <div className="flex-1">
-          <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">
-            Fund
-          </h4>
-
-          <div className="mt-[20px] flex flex-col p-4 bg-[#1c1c24] rounded-[10px]">
-            <p className="font-epilogue fount-medium text-[20px] leading-[30px] text-center text-[#808191]">
-              Fund the campaign
-            </p>
-            <div className="mt-[30px]">
-              <input
-                required
-                type="number"
-                placeholder="ETH 0.1"
-                step="0.01"
-                className="w-full py-[10px] sm:px-[20px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[18px] leading-[30px] placeholder:text-[#4b5264] rounded-[10px]"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-              />
-
-              <div className="my-[20px] p-4 bg-[#13131a] rounded-[10px]">
-                <h4 className="font-epilogue font-semibold text-[14px] leading-[22px] text-white">
-                  Back it because you believe in it.
-                </h4>
-                <p className="mt-[20px] font-epilogue font-normal leading-[22px] text-[#808191]">
-                  Support the project for no reward, just because it speaks to
-                  you.
-                </p>
-              </div>
-
-              <CustomButton
-                btnType="button"
-                title="Fund Campaign"
-                styles="w-full bg-[#8c6dfd]"
-                handleClick={handleDonate}
-              />
             </div>
           </div>
         </div>
