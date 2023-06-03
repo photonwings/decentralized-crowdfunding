a6d816ece990c60acc771038ad31c47d35e00f09
photonwings <vaibhavhathwar404@gmail.com>
2023-06-03 15:37:32 +0530

    Removed temp from gitignore

---
+++ client/src/temp/data.js
@@ -0,0 +1,170 @@
+export const tempDonation = [
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+  {
+    donation: "0.001",
+    nickName: "PhotonWings",
+    donator: "0xfB5663bA67ae5E80Db8Fcb80B131eEeBc04dc5d7",
+  },
+];
+
+export const tempComment = [
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project. This is good project. This is good project. This is good project. This is good project. This is good project. This is good project. This is good project. This is good project.",
+    profilePic: "",
+    isOwner: true,
+    dateOfComment: "12/12/2023",
+  },
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project",
+    profilePic: "",
+    isOwner: false,
+    dateOfComment: "12/12/2023",
+  },
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project",
+    profilePic: "",
+    isOwner: true,
+    dateOfComment: "12/12/2023",
+  },
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project",
+    profilePic: "",
+    isOwner: false,
+    dateOfComment: "12/12/2023",
+  },
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project",
+    profilePic: "",
+    isOwner: true,
+    dateOfComment: "12/12/2023",
+  },
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project",
+    profilePic: "",
+    isOwner: false,
+    dateOfComment: "12/12/2023",
+  },
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project",
+    profilePic: "",
+    isOwner: true,
+    dateOfComment: "12/12/2023",
+  },
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project",
+    profilePic: "",
+    isOwner: false,
+    dateOfComment: "12/12/2023",
+  },
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project",
+    profilePic: "",
+    isOwner: true,
+    dateOfComment: "12/12/2023",
+  },
+  {
+    nickName: "PhotonWings",
+    comment: "This is good project",
+    profilePic: "",
+    isOwner: false,
+    dateOfComment: "12/12/2023",
+  },
+];
